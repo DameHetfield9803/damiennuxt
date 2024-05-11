ab39ba975bbdd47eb7c4215ece6b43df0d1a5df4
DameHetfield9803 <damiendkfoo@yahoo.com.sg>
2024-05-12 01:25:19 +0800

    minor amendments

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-
+import "../index.css"
 </script>
 
 <template>
-  <div class="min-w-full container p-8 flex-auto text-center mx-auto min-h-screen bg-gradient-to-r from-black to-stone-800">
+  <div
+    class="min-w-full container p-8 flex-auto text-center mx-auto min-h-screen bg-gradient-to-r from-black to-slate-800">
     <div class="container border-4 min-w-fit flex-auto mx-auto text-center rounded-2xl border-violet-700 p-8">
-      <h1><strong class="text-6xl text-pink-400">Hi! Welcome to <span class="text-lime-400">INSERT COMPANY!</span></strong></h1>
+      <h1><strong class="text-6xl text-pink-400">Hi! Welcome to <span class="text-lime-400">INSERT
+            COMPANY!</span></strong></h1>
     </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
